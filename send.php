a4707fd56fb0f1fa7e9d35e30ebeaaecf403015e
Chornak`m <chornak17@gmail.com>
2021-03-23 14:15:56 +0100

    fix:modal & php

--- send.php
+++ send.php
@@ -11,13 +11,87 @@ $email = $_POST["email"];
 
 
 //формирование самого письма
+// Настройки
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try{
+  $mail->isSMTP();
+  $mail->CharSet ="UTF-8";
+  $mail->SMTPAuth = true;
+  $mail->SMTPDebug = 2;
+  $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+  $mail->isHTML(true);
+
+if (!empty($name) and !empty($phone) and !empty($email) and !empty($message)){
+  $title = "Новая бронь";
+  $body = "
+  <h2>Новый заказ на бронь</h2>
+  <b>Имя:</b> $name<br>
+  <b>Телефон:</b> $phone<br>
+  <b>Имейл:</b> $email<br>
+  <b>Сообщение:</b><br>$message
+";
+}
+elseif (!empty($email) and empty($name)) {
+  $title = "Новая подписка";
+  $body = "
+  <h2>Новое Имейл</h2>
+  <b>Имейл:</и> $email<br>
+  ";
+} 
+elseif (!empty($name) and !empty($phone) and !empty($message) and empty($email)) {
+  $title = "Новое обращение";
+  $body = "
+  <h2>Новое сообщение</h2>
+  <b>Имя:</b> $name<br>
+  <b>Телефон:</b> $phone<br>
+  <b>Сообщение:</b><br>$message
+";
+}
+
+  //настройки вашей почты
+  $mail->Host        ='smtp.mail.ru';
+  $mail->Username    ='m.chornak@mail.ru';
+  $mail->Password    ='Mm87655387';
+  $mail->SMTPSecure  ='ssl';
+  $mail->Port        = 465;
+  $mail->setFrom('m.chornak@mail.ru', 'Миша Чорнак');  
+
+  $mail->addAddress('chornak17@gmail.com');
+
+  // Письмо
+  $mail->isHTML(true); 
+  $mail->Subject = $title; // Заголовок письма
+  $mail->Body = $body; // Текст письма
+
+
+  // Результат
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: thankyou.html');
+?>
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+// Переменные
+
+
+
+
+//формирование самого письма
 $title = "Новое обращение Best Tour Plan";
 $body = "
-<h2>Новое письмо</h2>
-<b>Имя:</b> $name<br>
-<b>Телефон:</b> $phone<br>
+<h2>Новая подписка на рассылку/h2>
 <b>Имейл:</b> $email<br>
-<b>Сообщение:</b><br>$message
+
 ";
 // Настройки
 $mail = new PHPMailer\PHPMailer\PHPMailer();
@@ -27,6 +101,7 @@ try{
   $mail->SMTPAuth = true;
   $mail->SMTPDebug = 2;
   $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+  $mail->IsHTML(true);
 
   //настройки вашей почты
   $mail->Host        ='smtp.mail.ru';
