d6d9ba0ac6839eec48284bc2449e45c8125ff374
Chornak`m <chornak17@gmail.com>
2021-03-24 09:56:00 +0100

    fix: adaptation

--- js/main.js
+++ js/main.js
@@ -1,81 +1,58 @@
 $(document).ready(function () {
-  var hotelSlider = new Swiper(".hotel-slider", {
-    // Optional parameters
-    loop: true,
-
-    // Navigation arrows
+  new Swiper(".hotel-slider", {
+    loop: !0,
     navigation: {
       nextEl: ".hotel-slider__button--next",
       prevEl: ".hotel-slider__button--prev",
     },
-
-    keyboard: {
-      enabled: true,
-      onlyInViewport: false,
-    },
-
-    autoplay: {
-      delay: 3000,
-      stopOnLastSlide: false,
-      diasableOnInteraction: false,
-    },
-  });
-  var reveiwsSlider = new Swiper(".reviews-slider", {
-    // Optional parameters
-    loop: true,
-
-    // Navigation arrows
-    navigation: {
-      nextEl: ".reviews-slider__button--next",
-      prevEl: ".reviews-slider__button--prev",
-    },
-  });
-  var menuButton = $(".menu-button");
-  menuButton.on("click", function () {
-    $(".navbar-bottom").toggleClass("navbar-bottom-visible");
-    document.body.classList.toggle("lock");
-  });
-
-  var modalButton = $("[data-toggle=modal]");
-  var closeModalButton = $(".modal__close");
-  modalButton.on("click", openModal);
-  closeModalButton.on("click", closeModal);
-
-  function openModal() {
-    var targetModal = $(this).attr("data-href");
-    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
-    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
-    document.body.classList.toggle("lock");
-  }
-
-  function closeModal(event) {
-    event.preventDefault();
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-    modalOverlay.removeClass("modal__overlay--visible");
-    modalDialog.removeClass("modal__dialog--visible");
-    document.body.classList.remove("lock");
-  }
-  //обработка форм
-  $(".form").each(function () {
-    $(this).validate({
-      errorClass: "invalid",
-      messages: {
-        name: {
-          required: "Please enter your name",
-          minlength: "Name must be at least 2 letters long",
-        },
-        email: {
-          required: "We need your email address to contact you",
-          email: "Your email address must be in the format of name@domain.com",
-        },
-        phone: {
-          required: "The phone is deactivated",
-        },
+    keyboard: { enabled: !0, onlyInViewport: !1 },
+    autoplay: { delay: 3e3, stopOnLastSlide: !1, diasableOnInteraction: !1 },
+  }),
+    new Swiper(".reviews-slider", {
+      loop: !0,
+      navigation: {
+        nextEl: ".reviews-slider__button--next",
+        prevEl: ".reviews-slider__button--prev",
       },
     });
+  $(".menu-button").on("click", function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom-visible"),
+      document.body.classList.toggle("lock");
   });
-  $("#phone").mask("+7(999) 999-9999");
-  $("#phonex").mask("+7(999) 999-9999");
-  AOS.init();
+  var e = $("[data-toggle=modal]"),
+    o = $(".modal__close");
+  e.on("click", function () {
+    var e = $(this).attr("data-href");
+    $(e).find(".modal__overlay").addClass("modal__overlay--visible"),
+      $(e).find(".modal__dialog").addClass("modal__dialog--visible"),
+      document.body.classList.toggle("lock");
+  }),
+    o.on("click", function (e) {
+      e.preventDefault();
+      var o = $(".modal__overlay"),
+        e = $(".modal__dialog");
+      o.removeClass("modal__overlay--visible"),
+        e.removeClass("modal__dialog--visible"),
+        document.body.classList.remove("lock");
+    }),
+    $(".form").each(function () {
+      $(this).validate({
+        errorClass: "invalid",
+        messages: {
+          name: {
+            required: "Please enter your name",
+            minlength: "Name must be at least 2 letters long",
+          },
+          email: {
+            required: "We need your email address to contact you",
+            email:
+              "Your email address must be in the format of name@domain.com",
+          },
+          phone: { required: "The phone is deactivated" },
+        },
+      });
+    }),
+    $("#phone").mask("+7(999) 999-9999"),
+    $("#phonex").mask("+7(999) 999-9999"),
+    AOS.init();
 });
